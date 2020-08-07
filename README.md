c79466f78deb03cf10f6d77836fddcfa360198da
WorldTech <worldtechsolutions32@gmail.com>
2020-08-07 15:34:32 -0500

    initial guessing-game2 deploy

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # guessing-game2
+
+This time you are able to choos the number range in which you would like the program to pick one number from, then you can see how long it takes to guess the correct number. 
+
+This will require you to run it on your michene and not a web browser because it uses the sys module. 
+
+Clone repo, enter project folder via terminal, run ~ python3 g2.py num1 num2 ~ press enter and your michene will ask you to guess a number... Good Luck!
---
+++ g2.py
@@ -0,0 +1,20 @@
+from random import randint
+# you will need to run this on your machine and not this website for the sys.argv to work!
+import sys
+
+answer = randint(int(sys.argv[1]), int(sys.argv[2]))
+
+while True:
+    try:
+        guess = int(input(f'guess a number {sys.argv[1]}~{sys.argv[2]}:  '))
+        if 0 < guess < int(sys.argv[2]):
+            if guess == answer:
+                print('you are a genius!')
+                break
+        else:
+            g1 = {sys.argv[1]}
+            g2 = {sys.argv[2]}
+            print('hey bozo, you said', g1, "through", g2, "guess again!")
+    except ValueError:
+        print('please enter a number')
+        continue
