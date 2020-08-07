4fb7a1c7d1c0f904cb07dfaebe2ec0e4f374615c
WorldTech <worldtechsolutions32@gmail.com>
2020-08-07 15:49:55 -0500

    updated program

--- g2.py
+++ g2.py
@@ -7,14 +7,14 @@ answer = randint(int(sys.argv[1]), int(sys.argv[2]))
 while True:
     try:
         guess = int(input(f'guess a number {sys.argv[1]}~{sys.argv[2]}:  '))
-        if 0 < guess < int(sys.argv[2]):
+        if int(sys.argv[1]) < guess < int(sys.argv[2]):
             if guess == answer:
                 print('you are a genius!')
                 break
         else:
-            g1 = {sys.argv[1]}
-            g2 = {sys.argv[2]}
-            print('hey bozo, you said', g1, "through", g2, "guess again!")
+            g1 = int(sys.argv[1])
+            g2 = int(sys.argv[2])
+            print('hey bozo, you said', g1, '~', g2, "guess again!")
     except ValueError:
         print('please enter a number')
         continue
